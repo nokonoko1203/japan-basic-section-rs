6607e87c62af72ef1e5af354c5179d8a33f3ff12
nokonoko1203 <healtheworld.1203@gmail.com>
2023-10-16 23:47:24 +0900

    正しいと思われる形に変更

--- src/base_section.rs
+++ src/base_section.rs
@@ -6,7 +6,6 @@ use geo_types::Polygon;
 
 #[derive(Debug)]
 pub struct BaseSection {
-    number: i32,
     system_name: PlaneRectangularCoordinateSystem,
     origin: Origin,
     bbox: [i32; 4],
@@ -24,7 +23,9 @@ pub struct GridCell {
 }
 
 impl BaseSection {
-    pub fn new(number: i32, system_name: PlaneRectangularCoordinateSystem, origin: Origin) -> Self {
+    pub fn new(system_name: PlaneRectangularCoordinateSystem) -> Self {
+        let system_name = system_name;
+        let origin = system_name.origin();
         let bbox = [-160000, -300000, 160000, 300000];
         let top_left = (bbox[0], bbox[3]);
         let x_grid = 8;
@@ -33,9 +34,8 @@ impl BaseSection {
             (bbox[2] - bbox[0]) as f64 / x_grid as f64,
             (bbox[3] - bbox[1]) as f64 / y_grid as f64,
         );
-        let grid = Self::get_grid(number, top_left, grid_size, x_grid, y_grid);
+        let grid = Self::get_grid(system_name, top_left, grid_size, x_grid, y_grid);
         Self {
-            number,
             system_name,
             origin,
             bbox,
@@ -48,18 +48,19 @@ impl BaseSection {
     }
 
     fn get_grid(
-        number: i32,
+        system_name: PlaneRectangularCoordinateSystem,
         top_left: (i32, i32),
         grid_size: (f64, f64),
         x_grid: i32,
         y_grid: i32,
     ) -> Vec<GridCell> {
         let mut grid = Vec::new();
+
         for j in 0..y_grid {
             for i in 0..x_grid {
                 let index = format!(
-                    "{:02}{}{}",
-                    number,
+                    "{:?}{}{}",
+                    system_name,
                     (b'A' + j as u8) as char,
                     (b'A' + i as u8) as char
                 );
